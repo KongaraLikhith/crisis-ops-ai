ded9958b061bc8f316ddd4abab8d8cbfa97fc701
Mayank Porwal <120276362+mayank-porwal-da@users.noreply.github.com>
2026-04-05 03:11:01 +0530

    Update test_db.py

--- backend/test_db.py
+++ backend/test_db.py
@@ -4,18 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Instead of passing the whole string to create_engine, 
-# we build a URL object which handles the "@" automatically.
-url_object = URL.create(
-    drivername="postgresql",
-    username="postgres",
-    password="24VaYOhV9QSmZbMm@",  # You can pull this from os.getenv("DB_PASS")
-    host="db.dwggagcfzwhxswaelbqt.supabase.co",
-    port=5432,
-    database="postgres"
-)
-
-engine = create_engine(url_object)
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
 
 with engine.connect() as conn:
     result = conn.execute(text("SELECT 1"))
